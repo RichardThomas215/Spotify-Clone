8b30b12f25d292c081862413bbf726f81ba77128
RichardThomas215 <richardthomas215@yahoo.com>
2022-01-23 18:20:11 -0500

    added spotify.js and added spotifys authentication

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css';
+import { loginUrl } from './spotify';
 
 function Login(){
 
@@ -8,7 +9,7 @@ function Login(){
            <h1>I am the login page</h1>
            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify logo"/>
 
-           <a>LOGIN WITH SPOTIFY</a>
+           <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
