e47359307fa13ba7066776b0ffd4ae64f3b9ad92
RichardThomas215 <richardthomas215@yahoo.com>
2022-01-23 22:42:27 -0500

    switched from using states to data layer and began working on spotify home page

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "./Player.css";
+import Sidebar from './Sidebar';
 
-function Player() {
+function Player({ spotify }) {
   return (
-        <div>
-            <h1>Welcome to Spotify</h1>
+        <div className="player">
+            <div className="player_body">
+              <Sidebar/>
+            </div>
+
+
        </div>
   )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ export const initialState = {
     user: null,
     playlist: [],
     playing: false,
-    item: null
+    item: null,
+    //REMOVE AFTER DEVELOPING
+    token: 'BQCOlXJfr583_jYBTwLm3adgiPnX5yBmgtPgkpEkyaXAjlFWbGfaXjFYDCBNrzlpMyvgv4C3FEQJGRTwhPl3QNq8W352ur0YWoPKekhF7GhvIVkKYkcjMkXX_EDUr8ChNzvN7S3VYaLUgP0KefsAK9KZUxnGx9g',
 };
 
 const reducer = (state, action) => {
@@ -12,8 +14,14 @@ console.log(action);
         case 'SET_USER':
             return {
                 ...state,
-                user: action.user
-            }
+                user: action.user,
+            };
+        
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token,
+            };
         default:
             return state;
     }
