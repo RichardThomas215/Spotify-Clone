3171c48fc5298ae29ac74a5d3a4bf8a0f32b56d2
RichardThomas215 <richardthomas215@yahoo.com>
2022-01-23 19:05:10 -0500

    finished login authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const[token, setToken] = useState(null);
+
+  //Run code based on a given condition
+  useEffect(() =>{
+      const hash = getTokenFromUrl();
+      window.location.hash = "";
+      const _token = hash.access_token;
+
+      if(_token){
+        setToken(_token)
+      }
+
+      console.log("I HAVE A TOKEN>>> ", token);
+  }, []);
+
   return (
-    //BEM
     <div className="app">
-      <Login/>
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login/>
+        )
+      }
+     
 
     </div>
   );
