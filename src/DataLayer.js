b456386a25b996ebf895c453b784065fcaf422c5
RichardThomas215 <richardthomas215@yahoo.com>
2022-01-23 20:01:23 -0500

    setting up datalayer for user

---
+++ src/DataLayer.js
@@ -0,0 +1,14 @@
+import React, 
+{ createContext, 
+    userContext, 
+    useReducer}  
+    
+    from "react";
+
+    export const DataLayerContext = createContext();
+
+    export  const DataLayer = ({ initialState, reducer, children}) => (
+            <DataLayerContext.Provider value={useReducer(reducer,initialState)}>
+                {children}
+            </DataLayerContext.Provider>
+    )
---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+export const intialState = {
+    user: null,
+    playlist: [],
+    playing: false,
+    item: null
+};
+
+const reducer = (state, action) => {
+console.log(action);
+
+}
+
+export default reducer;
