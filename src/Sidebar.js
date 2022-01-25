c8e6d6727f96f07cd3369d23bc4bcafa629cdaa6
RichardThomas215 <richardthomas215@yahoo.com>
2022-01-25 10:40:45 -0500

    Major work on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import "./Sidebar.css";
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
   return (
   
-    <div>
-        <h1>I am the Sidebar</h1>
+    <div className="sidebar">
+        <img className="sidebar__logo"src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="img"/>
+        
+        <SidebarOption title="Home"/>
+        <SidebarOption title="Search"/>
+        <SidebarOption title="Your Library"/>
+
     </div>
 
   );
